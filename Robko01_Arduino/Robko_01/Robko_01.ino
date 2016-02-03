da9e10aff21ab50599d23a10d2cedd23a0c4d257
hayyvord <hayyvord@gmail.com>
2016-02-03 08:50:23 +0200

    Robko01

--- Robko01_Arduino/Robko_01/Robko_01.ino
+++ Robko01_Arduino/Robko_01/Robko_01.ino
@@ -2,7 +2,7 @@
 #include <String.h>
 #include <stdlib.h>
 
-AccelStepper Base(AccelStepper::FULL4WIRE, 23, 25, 27, 29);
+AccelStepper Base(AccelStepper::HALF4WIRE, 23, 25, 27, 29);
 AccelStepper Shoulder(AccelStepper::HALF4WIRE, 22, 24, 26, 28);
 AccelStepper Elbow(AccelStepper::HALF4WIRE, 30, 32, 34, 36);
 AccelStepper Pitch(AccelStepper::HALF4WIRE, 39, 41, 43, 45);
@@ -48,17 +48,18 @@ boolean ValidateCommand(String command)
 
   int Motor = 0;
   int Steps = 0;
+  int Speed = 0;
 
-
-  //?D1-0100\n
-  if (command[0] == '?' && command[8] == '\n')
+  //?M0+1000:0123\n
+  if (command[0] == '?' && command[8] == ':' && command[13] == '\n')
   {
     if (command[3] == '-' || command[3] == '+')
     {
-      if (command[1] == 'M')
+      if (command[1] == 'D')
       {
         Motor = atoi(command.substring(2).c_str());
         Steps = atoi(command.substring(4, 8).c_str());
+        Speed = atoi(command.substring(9, 13).c_str());
 
         if (Steps <= 1999 && Steps >= -1999)
         {
@@ -70,28 +71,25 @@ boolean ValidateCommand(String command)
           state = true;
         }
 
+        if (Speed >= 0 && Speed <= 100)
+        {
+          state = true;
+        }
       }
     }
   }
 
-
-  if (command[0] == '?' && command[3] == '\n')
-  {
-    if (command[1] == 'D')
-    {
-      Motor = atoi(command.substring(2).c_str());
-
-      if (Motor >= 0 && Motor <= 5)
-      {
-        state = true;
-      }
-    }
-  }
-
-  if (command[0] == '?' && command[3] == '\n')
+  //?E0000\n
+  if (command[0] == '?' && command[2] == ':' && command[7] == '\n')
   {
     if (command[1] == 'E')
     {
+      Speed = atoi(command.substring(3, 7).c_str());
+
+      if (Speed >= 0 && Speed <= 100)
+      {
+        state = true;
+      }
       Motor = atoi(command.substring(2).c_str());
 
       if (Motor >= 0 && Motor <= 5)
@@ -100,6 +98,7 @@ boolean ValidateCommand(String command)
       }
     }
   }
+
 
   if (command[0] == '?' && command[3] == '\n')
   {
@@ -123,31 +122,42 @@ void ParseCommand(String command)
 
   int Motor = 0;
   int Steps = 0;
+  int Speed = 0;
 
-  if (command[0] == '?' && command[8] == '\n')
+
+  if (command[0] == '?' && command[8] == ':' && command[13] == '\n')
   {
     if (command[3] == '-' || command[3] == '+')
     {
-      Motor = atoi(command.substring(2).c_str());
-      Steps = atoi(command.substring(4, 8).c_str());
-
-      if (command[1] == 'M')
+      if (command[1] == 'D')
       {
+        Motor = atoi(command.substring(2).c_str());
+        Steps = atoi(command.substring(4, 8).c_str());
+        Speed = atoi(command.substring(9, 13).c_str());
 
         if (command[3] == '+')
         {
-          RunStepper(Motor, Steps);
+          RunStepper(Motor, Steps, Speed);
         }
         else if (command[3] == '-')
         {
-          RunStepper(Motor, -Steps);
+          RunStepper(Motor, -Steps, Speed);
         }
-
-        //RunStepper(Motor, Steps);
 
       }
     }
   }
+  //E0:1000\n
+  else if (command[0] == '?' && command[2] == ':' && command[7] == '\n')
+  {
+    if (command[1] == 'E')
+    {
+      Speed = atoi(command.substring(3, 7).c_str());
+      Motor = atoi(command.substring(2).c_str());
+      SetSpeed(Motor, Speed);
+    }
+  }
+
 
   else if (command[0] == '?' && command[3] == '\n')
   {
@@ -160,51 +170,68 @@ void ParseCommand(String command)
     }
   }
 
-  else if (command[0] == '?' && command[3] == '\n')
-  {
-    if (command[1] == 'E')
-    {
 
-      Motor = atoi(command.substring(2).c_str());
-      //enableMotors(Motor);
-
-    }
-  }
-
-  else if (command[0] == '?' && command[3] == '\n')
-  {
-    if (command[1] == 'D')
-    {
-
-      Motor = atoi(command.substring(2).c_str());
-      //disableMotors(Motor);
-
-    }
-  }
 }
 
-void RunStepper(int motor, int steps)
+void RunStepper(int motor, int steps, int Speed)
 {
   switch (motor) {
     case 0:
       Base.move(steps);
+      Base.setSpeed(Speed);
       break;
     case 1:
       Shoulder.move(steps);
+      Shoulder.setSpeed(Speed);
       break;
     case 2:
       Elbow.move(steps);
+      Elbow.setSpeed(Speed);
       break;
     case 3:
       Pitch.move(steps);
+      Pitch.setSpeed(Speed);
       Roll.move(steps);
+      Roll.setSpeed(Speed);
       break;
     case 4:
       Pitch.move(-steps);
+      Pitch.setSpeed(Speed);
       Roll.move(steps);
+      Roll.setSpeed(Speed);
       break;
     case 5:
       Gripper.move(steps);
+      Gripper.setSpeed(Speed);
+      break;
+    default:
+
+      break;
+  }
+}
+
+void SetSpeed(int motor, int Speed)
+{
+  switch (motor) {
+    case 0:
+      Base.setSpeed(Speed);
+      break;
+    case 1:
+      Shoulder.setSpeed(Speed);
+      break;
+    case 2:
+      Elbow.setSpeed(Speed);
+      break;
+    case 3:
+      Pitch.setSpeed(Speed);
+      Roll.setSpeed(Speed);
+      break;
+    case 4:
+      Pitch.setSpeed(Speed);
+      Roll.setSpeed(Speed);
+      break;
+    case 5:
+      Gripper.setSpeed(Speed);
       break;
     default:
 
@@ -217,23 +244,31 @@ void StopSteppers(int motor)
   switch (motor) {
     case 0:
       Base.stop();
+      Base.setSpeed(0.00);
       break;
     case 1:
       Shoulder.stop();
+      Shoulder.setSpeed(0.00);
       break;
     case 2:
       Elbow.stop();
+      Elbow.setSpeed(0.00);
       break;
     case 3:
       Pitch.stop();
+      Pitch.setSpeed(0.00);
       Roll.stop();
+      Roll.setSpeed(0.00);
       break;
     case 4:
       Pitch.stop();
+      Pitch.setSpeed(0.00);
       Roll.stop();
+      Roll.setSpeed(0.00);
       break;
     case 5:
       Gripper.stop();
+      Gripper.setSpeed(0.00);
       break;
     default:
 
@@ -245,26 +280,31 @@ void StopSteppers(int motor)
 void SetupSteppers()
 {
   Base.setMaxSpeed(100.0);
-  Base.setAcceleration(100.0);
+  Base.setSpeed(100.0);
+
   Shoulder.setMaxSpeed(100.0);
-  Shoulder.setAcceleration(100.0);
+  Shoulder.setSpeed(100.0);
+
   Elbow.setMaxSpeed(100.0);
-  Elbow.setAcceleration(100.0);
+  Elbow.setSpeed(100.0);
+  
   Pitch.setMaxSpeed(100.0);
-  Pitch.setAcceleration(100.0);
+  Pitch.setSpeed(100.0);
+  
   Roll.setMaxSpeed(100.0);
-  Roll.setAcceleration(100.0);
+  Roll.setSpeed(100.0);
+  
   Gripper.setMaxSpeed(100.0);
-  Gripper.setAcceleration(100.0);
+  Gripper.setSpeed(100.0);
 }
 
 void RunMotors()
 {
-  Base.run();
+  Base.runSpeedToPosition();
   Shoulder.run();
   Elbow.run();
   Pitch.run();
   Roll.run();
-  Gripper.run();  
+  Gripper.run();
 }
 
